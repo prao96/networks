efaff3acd21bddde407f3908e2d3a9e82b037656
Sam Girvin <sam.girvin@gmail.com>
2013-04-15 22:55:05 -0700

    Fixed all of the indentation

--- source.py
+++ source.py
@@ -5,65 +5,67 @@ from graphs import *
 import binascii
 import random
 import base64
+import pdb
 
 
 class Source:
-    def __init__(self, monotone, filename=None):
-        # The initialization procedure of source object
-        self.monotone = monotone
-        self.fname = filename
-        print 'Source: '
+	def __init__(self, monotone, filename=None):
+		# The initialization procedure of source object
+		self.monotone = monotone
+		self.fname = filename
+		print 'Source: '
 
-    def process(self):
-            # Form the databits, from the filename 
-            if self.fname is not None:
-                if self.fname.endswith('.png') or self.fname.endswith('.PNG'):
-                   databits = bits_from_image(self, self.fname)
-                else:           
-                    databits = text2bits(self, self.fname)  
-				    length = len(databits)               
-            else:               
-                databits = np.ones(1000)
-			    header = get_header(self, len(databits), '1')
-			    databits = binascii.a2b_qp(header) + databits
-            return payload, databits
+	def process(self):
+		# Form the databits, from the filename 
+		#pdb.set_trace()
+		if self.fname is not None:
+			if self.fname.endswith('.png') or self.fname.endswith('.PNG'):
+				databits = bits_from_image(self, self.fname)
+			else:
+				databits = text2bits(self, self.fname)  
+				length = len(databits)               
+		else:
+			databits = np.ones(1000)
+			header = get_header(self, len(databits), '1')
+			databits = binascii.a2b_qp(header) + databits
+		return payload, databits
 
-    def text2bits(self, filename):
+	def text2bits(self, filename):
 		file = open(filename)
 		while 1:
-        # Form the databits, from the filename 
-        if self.fname is not None:
-            if self.fname.endswith('.png') or self.fname.endswith('.PNG'):
-                databits = bits_from_image(self, self.fname)
-            else:           
-                databits = text2bits(self, self.fname)  
-                length = len(databits)               
-        else:               
-            databits = np.ones(1000)
-            header = get_header(self, len(databits), '1')
-            databits = binascii.a2b_qp(header) + databits
-        return payload, databits
+			# Form the databits, from the filename 
+			if self.fname is not None:
+				if self.fname.endswith('.png') or self.fname.endswith('.PNG'):
+					databits = bits_from_image(self, self.fname)
+				else:
+					databits = text2bits(self, self.fname)  
+					length = len(databits)               
+			else:
+				databits = np.ones(1000)
+				header = get_header(self, len(databits), '1')
+				databits = binascii.a2b_qp(header) + databits
+		return payload, databits
 
-    def text2bits(self, filename):
-        file = open(filename)
-        while 1:
-            line = file.readline()
-            if not line:
-        		break
-    		else:
-			    str = binascii.a2b_uu(line)
-			    bits = binascii.a2b_qp(str)
-		        length = len(bits)
-		        header = get_header(self, length, '01')
-		        bits = binascii.a2b_qp(header + str)
+	def text2bits(self, filename):
+		file = open(filename)
+		while 1:
+			line = file.readline()
+			if not line:
+				break
+			else:
 				str = binascii.a2b_uu(line)
 				bits = binascii.a2b_qp(str)
-		        length = len(bits)
-		        header = get_header(self, length, '01')
-		        bits = binascii.a2b_qp(header + str)
-        return bits
+				length = len(bits)
+				header = get_header(self, length, '01')
+				bits = binascii.a2b_qp(header + str)
+				str = binascii.a2b_uu(line)
+				bits = binascii.a2b_qp(str)
+				length = len(bits)
+				header = get_header(self, length, '01')
+				bits = binascii.a2b_qp(header + str)
+		return bits
 
-    def bits_from_image(self, filename):
+	def bits_from_image(self, filename):
 		file = open(filename)
 		image = file
 		image_64 = base64.encodestring(open(image, "l").read())
@@ -72,14 +74,14 @@ class Source:
 		length = len(bits)
 		header = get_header(self, length, '01')
 		bits = binascii.a2b_qp(header + str)
-        return bits
+		return bits
 
-    def get_header(self, payload_length, srctype): 
-  		if self.fname is not None:
-                if self.fname.endswith('.png') or self.fname.endswith('.PNG'):
-                    header = srctype + "001" + str(payload_length)
-                else:           
-                    header = srctype + "000" + str(payload_length)   
+	def get_header(self, payload_length, srctype): 
+		if self.fname is not None:
+				if self.fname.endswith('.png') or self.fname.endswith('.PNG'):
+					header = srctype + "001" + str(payload_length)
+				else:
+					header = srctype + "000" + str(payload_length)   
 		else:
 			header = srctype + "111" + str(payload_length)    
-        return header
+		return header
